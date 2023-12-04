09ddeb77c3f035a200dbf9337af6463144563dfe
place4code <place4code@gmail.com>
2023-12-04 15:09:16 +0100

    Init commit

---
+++ src/main/java/com/place4code/clone/CloneApplication.java
@@ -0,0 +1,13 @@
+package com.place4code.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/place4code/clone/CloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.place4code.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
