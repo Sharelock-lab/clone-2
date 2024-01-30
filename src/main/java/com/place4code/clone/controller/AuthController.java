94cfd70533f4ac5b7ccf8237462f3be6e6960e63
place4code <place4code@gmail.com>
2024-01-30 11:38:25 +0100

    Formularz Rejestracji

--- src/main/java/com/place4code/clone/controller/AuthController.java
+++ src/main/java/com/place4code/clone/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.place4code.clone.controller;
 
+import com.place4code.clone.model.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,6 +13,12 @@ public class AuthController {
     @GetMapping("/login")
     String login() {
         return "auth/login";
+    }
+
+    @GetMapping("/register")
+    String registerView(final Model model) {
+        model.addAttribute("user", new User());
+        return "auth/register";
     }
 
 }
